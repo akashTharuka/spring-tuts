feee8cee00fff5428b8e8578fd2a291c55e28727
akashTharuka <akash_tharuka@yahoo.com>
2024-09-16 17:23:12 +0530

    first commit

---
+++ src/test/java/com/tuts/springTuts/SpringTutsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuts.springTuts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
